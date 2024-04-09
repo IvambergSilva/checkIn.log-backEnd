bd50644f6763bcf6d2a70dde355c8f231b7fe2dc
IvambergSilva <ivambergisilva@gmail.com>
2024-04-09 12:08:03 -0300

    :sparkles: feat: Conexão com o PostgreSQL

--- prisma/migrations/20240409150259_tables_creation_with_postgre_sql/migration.sql
+++ prisma/migrations/20240409150259_tables_creation_with_postgre_sql/migration.sql
@@ -1,15 +1,17 @@
 -- CreateTable
 CREATE TABLE "events" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "details" TEXT,
     "slug" TEXT NOT NULL,
-    "maximum-attendees" INTEGER
+    "maximum-attendees" INTEGER,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "attendees" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "socialName" TEXT,
     "email" TEXT NOT NULL,
@@ -18,17 +20,19 @@ CREATE TABLE "attendees" (
     "customGender" TEXT,
     "treatAs" TEXT,
     "accessibility" TEXT,
-    "created-at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created-at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "event-id" TEXT NOT NULL,
-    CONSTRAINT "attendees_event-id_fkey" FOREIGN KEY ("event-id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "attendees_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "check-ins" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "created-at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" TEXT NOT NULL,
+    "created-at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "attendee-id" TEXT NOT NULL,
-    CONSTRAINT "check-ins_attendee-id_fkey" FOREIGN KEY ("attendee-id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "check-ins_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -39,3 +43,9 @@ CREATE UNIQUE INDEX "attendees_event-id_email_key" ON "attendees"("event-id", "e
 
 -- CreateIndex
 CREATE UNIQUE INDEX "check-ins_attendee-id_key" ON "check-ins"("attendee-id");
+
+-- AddForeignKey
+ALTER TABLE "attendees" ADD CONSTRAINT "attendees_event-id_fkey" FOREIGN KEY ("event-id") REFERENCES "events"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check-ins" ADD CONSTRAINT "check-ins_attendee-id_fkey" FOREIGN KEY ("attendee-id") REFERENCES "attendees"("id") ON DELETE CASCADE ON UPDATE CASCADE;
